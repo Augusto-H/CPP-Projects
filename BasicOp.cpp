5cf1063be46c0d3d3cfa720142b1359a287610ab
Prismagh0n <50850987+Augusto-H@users.noreply.github.com>
2019-12-03 21:22:00 -0300

    Create BasicOp.cpp

---
+++ BasicOp.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+// Programa para calcular dois valores inteiros //
+int main ()
+  {
+  	int x1, x2;
+  	x1 = 10;
+  	x2 = 20;
+  	
+  	std::cout << "x1 + x2 = " << x1 + x2 << "\n\n";
+  	system ("PAUSE");
+  	return 0;
+  }
