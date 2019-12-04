4869d410e466283a813cd44b2cb2f60ab6f00722
Prismagh0n <50850987+Augusto-H@users.noreply.github.com>
2019-12-03 21:22:53 -0300

    Create BasicOP2.cpp

---
+++ BasicOP2.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+// Programa para somar duas variáveis inteiras //
+
+int main()
+ {
+ 	int valor_1, valor_2;
+ 	std::cout << " \nDigite o primeiro valor e tecle Enter: \n";
+ 	std::cin >> valor_1;
+ 	std::cout << " \nDigite o segundo valor e tecle Enter: \n";
+ 	std::cin >> valor_2;
+ 	
+ 	std::cout << "Multi = " << valor_1 * valor_2 << "\n\n";
+ 	system ("PAUSE");
+ 	return 0;
+ 	
+ }
